0cca777d2de9f6606b3ad118fa18d34687f73a84
KSA <KSA@KSAPAPB00521.srza.local>
2016-08-24 14:40:51 +0200

    broke the createList function apart

--- src/Daniel/DatastructuresAndAlgorithms.java
+++ src/Daniel/DatastructuresAndAlgorithms.java
@@ -2,38 +2,44 @@ package Daniel;
 
 public class DatastructuresAndAlgorithms {
 
-    static final int TEST_FORWARD = 0;
-    static final int TEST_BACKWARD = 1;
-
     public static void main(String[] args) {
-        testDoubleLinkedList(0);
+        testDoubleLinkedList();
     }
 
-    public static void testDoubleLinkedList(int i) {
+    public static void testDoubleLinkedList() {
 
         DoubleLinkedList TestList = createList();
 
+        System.out.println("FORWARD : ");
         printListForward(TestList);
         System.out.println("BACKWARD : ");
         printListBackward(TestList);
 
     }
 
-    public static DoubleLinkedList createList() {
-
-        int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8};
-
+    public static DoubleLinkedList createEmptyList() {
+        
         DoubleLinkedList HeadSentinel = new DoubleLinkedList(); //head sentinel
         DoubleLinkedList TailSentinel = new DoubleLinkedList(); //tail sentinel
         HeadSentinel.next = TailSentinel;
         HeadSentinel.next.previous = HeadSentinel;
         HeadSentinel = HeadSentinel.next;
 
+        return HeadSentinel;
+    }
+    
+    public static DoubleLinkedList createList() {
+
+        int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        DoubleLinkedList emptyList = createEmptyList();
+        
+        
         for (int num : numbers) {
-            HeadSentinel.append(num);
+            emptyList.append(num);
         }
 
-        return HeadSentinel;
+        return emptyList;
     }
 
     public static void printListBackward(DoubleLinkedList element) {
