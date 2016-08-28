90db03b012761e7332b84f9d72a3560efc0645b4
Luky66 <lukasmc6@gmail.com>
2016-08-28 17:29:57 +0200

    added "insertAtEnd" and "insertAtBeginning"functions

--- src/Lukas/DoubleLinkedList.java
+++ src/Lukas/DoubleLinkedList.java
@@ -3,60 +3,84 @@ package Lukas;
 public class DoubleLinkedList<T>
 {
 
-    DoubleLinkedList<T> next;
-    T value;
-    DoubleLinkedList<T> previous;
+	DoubleLinkedList<T> next;
+	T value;
+	DoubleLinkedList<T> previous;
 
-    public DoubleLinkedList()
-    {
-    }
+	public DoubleLinkedList()
+	{
+	}
 
-    public void setValue(T item)
-    {
-        assert (next != null);
-        assert (previous != null);
-        this.value = item;
-    }
+	public void setValue(T item)
+	{
+		assert (next != null);
+		assert (previous != null);
+		this.value = item;
+	}
 
-    public T getValue()
-    {
-        assert (next != null);
-        assert (previous != null);
-        return value;
-    }
+	public T getValue()
+	{
+		assert (next != null);
+		assert (previous != null);
+		return value;
+	}
 
-    public boolean hasNextElement()
-    {
-        return next != null;
-    }
+	public boolean hasNextElement()
+	{
+		return next != null;
+	}
 
-    public boolean hasPreviousElement()
-    {
-        return previous != null;
-    }
+	public boolean hasPreviousElement()
+	{
+		return previous != null;
+	}
 
-    public DoubleLinkedList<T> getNextElement()
-    {
-        assert (next != null);
-        return next;
-    }
+	public DoubleLinkedList<T> getNextElement()
+	{
+		assert (next != null);
+		return next;
+	}
 
-    public DoubleLinkedList<T> getPreviousElement()
-    {
-        assert (previous != null);
-        return previous;
-    }
+	public DoubleLinkedList<T> getPreviousElement()
+	{
+		assert (previous != null);
+		return previous;
+	}
 
-    public void insertAfter(T newItem)
-    {
-        DoubleLinkedList<T> last = this; // This is listTail now(in this function)
+	public void insertAfter(T newItem)
+	{
+		DoubleLinkedList<T> last = this;
 
-        last.value = newItem;
-        last.next = new DoubleLinkedList<T>();
+		last.value = newItem;
+		last.next = new DoubleLinkedList<T>(); // adds sentinel
+		last.next.previous = last;
+	}
+	
+	public void insertAtEnd(T newItem)
+	{
+		DoubleLinkedList<T> last = this;
 
-        //This gives a great overview. Enable to see the linking of the List:
-        //System.out.println("Overview: previous=" + last.previous + "; current=" + last.value + "; next=" + last.next);
-
-        last.next.previous = last;
-    }
+		while(last.next != null)
+		{
+			last = last.next;
+		}
+		
+		last.value = newItem;
+		last.next = new DoubleLinkedList<T>();
+		last.next.previous = last;
+	}
+	
+	public void insertAtBeginning(T newItem)
+	{
+		DoubleLinkedList<T> last = this;
+		
+		while(last.previous != null)
+		{
+			last = last.previous;
+		}
+		
+		last.value = newItem;
+		last.previous = new DoubleLinkedList<T>();
+		last.previous.next = last;
+	}
 }
