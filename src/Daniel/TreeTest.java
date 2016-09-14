308f749d618687a0291e6ef6685d8154bf514e3e
meintte <KSA@KSAPAPB00518.srza.local>
2016-09-14 14:47:46 +0200

    added an update, but delete still doesn't work

--- src/Daniel/TreeTest.java
+++ src/Daniel/TreeTest.java
@@ -3,8 +3,30 @@ package Daniel;
 public class TreeTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int values[] = { 7, 4, 2, 6, 5, 10, 8, 15, 17, 11, 13 };
+		
+		TreeNode TestTree = TreeNode.createTree(values);
+		
+		/*
+		System.out.println("PreOrder: ");
+		TestTree.printPreOrder();
+		System.out.println("InOrder: ");
+		TestTree.printInOrder();
+		System.out.println("PostOrder: ");
+		TestTree.printPostOrder();
+		*/
+		
+		System.out.println("before, PreOrder: ");
+		TestTree.printPreOrder();
+		
+		TestTree.find(14).delete();
+		
+		System.out.println("deleted, PreOrder: ");
+		TestTree.printPreOrder();
+		//root = five.update(15);
+		
+		
+		
 	}
 
 }
