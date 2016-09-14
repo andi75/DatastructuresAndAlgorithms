8e093d18ef45c8b4510073b9ed51ae2827c718cf
KSA <KSA@KSAPAPB00518.srza.local>
2016-09-14 12:58:52 +0200

    worked on it, not finished, started to late...

--- src/Daniel/TreeNode.java
+++ src/Daniel/TreeNode.java
@@ -10,21 +10,17 @@ public class TreeNode {
 	}
 	
 	
-	
-	public TreeNode find(int value) {
+	public static TreeNode createTree(int[] values) {
+		TreeNode root = new TreeNode(values[0]);
 		
-		if(value < this.value && this.leftChild != null) {
-			return this.leftChild.find(value);
-		} else if(value <= this.value && this.rightChild != null) {
-			return this.rightChild.find(value);
+		for(int i = 1; i < values.length; i++) {
+			root.insert(values[i]);
 		}
 		
-		return null;
+		return root;
 	}
-
-
-
-	public void add(int v) {
+	
+	public void insert(int v) {
 		TreeNode currentNode = this;
 		TreeNode newNode = new TreeNode(v);
 		
@@ -37,7 +33,7 @@ public class TreeNode {
 			} 
 			else 
 			{
-				currentNode.leftChild.add(v);
+				currentNode.leftChild.insert(v);
 			}
 		}
 		else if(newNode.value >= currentNode.value)
@@ -49,66 +45,203 @@ public class TreeNode {
 			}
 			else
 			{
-				currentNode.rightChild.add(v);
+				currentNode.rightChild.insert(v);
 			}
 		}
 		
+	}
+	
+	
+	public TreeNode delete() {
 		
+		TreeNode root = this.getRoot();
+		
+		if(this.leftChild == null && this.rightChild == null) { //leaf
+						
+			if(this.parent.leftChild == this) {
+				this.parent.leftChild = null;
+				return root;
+			} else {
+				this.parent.rightChild = null;
+				return root;
+			}
+		} 
+		
+		else if(this.leftChild != null && this.rightChild == null) { //one child left
+		
+			if(this.parent.leftChild == this) {
+				this.parent.leftChild = this.leftChild;
+				return root;
+			} else {
+				this.parent.rightChild = this.leftChild;
+				return root;
+			}
+			
+		} else if(this.leftChild == null && this.rightChild != null) { // one child right
+			
+			if(this.parent.leftChild == this) {
+				this.parent.leftChild = this.rightChild;
+				return root;
+			} else {
+				this.parent.rightChild = this.rightChild;
+				return root;
+			}
+			
+			// two childs:
+			
+		} else if(this.leftChild != null && this.rightChild != null) {
+			
+			TreeNode mostLeft = this.rightChild.getMostLeftChild();
+			
+			if(mostLeft.rightChild == null) {
+			
+				mostLeft.parent = this.parent;
+				
+				if(this.parent != null) {
+					if(this.parent.leftChild == this) {
+						mostLeft.parent.leftChild = mostLeft;
+						
+					} else {
+						mostLeft.parent.rightChild = mostLeft;
+					}
+				}
+				
+				mostLeft.leftChild = this.leftChild;
+				
+				
+			} else {
+				TreeNode mostLeftRight = mostLeft.rightChild;
+				
+				
+				System.out.println("has to be done, started to late...");;
+				
+			}
+			
+		
+		}
+		
+		
+			
+		return null;
+		
+
+	}
+
+	public TreeNode find(int value) {
+		
+		TreeNode currentNode = this.getRoot();
+	
+		while(currentNode.value != value) {
+			
+			if(value < currentNode.value) {
+				if(currentNode.leftChild != null) {
+					currentNode = currentNode.leftChild;
+				} else {
+					return null;
+				}
+				
+			} else if(value > currentNode.value) {
+				if(currentNode.rightChild != null) {
+					currentNode = currentNode.rightChild;
+				} else {
+					return null;
+				}
+			}
+			
+			
+			
+			
+		}
+		
+		return currentNode;
+		
+	}
+
+
+	public void printInOrder() {
+
+		//prints left branch
+		if(this.leftChild != null) {
+			this.leftChild.printInOrder();
+		}
+		
+		//prints root
+		System.out.println(this.value);
+		
+		//prints right branch
+		if(this.rightChild != null) {
+			this.rightChild.printInOrder();
+		}
+
+	}
+	
+	
+	public void printPreOrder() {
+		
+		//prints root
+		System.out.println(this.value);		
+		
+		//prints left branch
+		if(this.leftChild != null) {
+			this.leftChild.printPreOrder();
+		}
+		
+		//prints right branch
+		if(this.rightChild != null) {
+			this.rightChild.printPreOrder();
+		}
+		
+	}
+
+	
+	public void printPostOrder() {
+		
+		//prints left branch
+		if(this.leftChild != null) {
+			this.leftChild.printPostOrder();
+		}
+					
+		//prints right branch
+		if(this.rightChild != null) {
+			this.rightChild.printPostOrder();
+		}
+		
+		//prints root
+		System.out.println(this.value);
+
 		
 	}
 	
 	public TreeNode getRoot() {
 		TreeNode currentNode = this;
 		
-		while(currentNode.parent != null) {
-			currentNode = currentNode.parent;
-		}
-		
-		return currentNode;
-	}
-	
-	
-	public void printInOrder() {
-		TreeNode currentNode = this;
-	
-		if(currentNode.leftChild != null) 
-		{
-			currentNode.leftChild.printInOrder();
-		} 
-		else if(currentNode.rightChild != null)
-		{
-			currentNode.rightChild.printInOrder();
+		if(currentNode.parent == null) {
+			return currentNode;
 		} else {
-			System.out.println(currentNode.value);
-			assert(currentNode.parent != null);
-			currentNode = currentNode.parent;
+			return currentNode.parent.getRoot();
 		}
 		
-		
+	}
 	
+	private TreeNode getMostLeftChild() {
+		TreeNode currentNode = this;
 		
-		
-	}
-
-
-
-	public void printPreOrder() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-	public void printPostOrder() {
-		// TODO Auto-generated method stub
+		if(currentNode.leftChild != null) {
+			return currentNode.leftChild.getMostLeftChild();
+		} else {
+			return currentNode;
+		}
 		
 	}
-
-
-
-	public TreeNode deleteWithNewRoot() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private TreeNode getMostRightChild() {
+		TreeNode currentNode = this;
+		
+		if(currentNode.rightChild != null) {
+			return currentNode.rightChild.getMostRightChild();
+		} else {
+			return currentNode;
+		}
 	}
 
 }
